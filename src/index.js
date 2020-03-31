e839f7e5ebc624e54044309282bbc64a5651939f
willian <hellowillian21@gmail.com>
2020-03-31 16:37:44 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+
+class Animal {
+  constructor(name) {
+    this.name = name
+  }
+
+  getName() {
+    return this.name
+  }
+}
+
+const dog = new Animal('dog')
+console.log(dog.getName())
